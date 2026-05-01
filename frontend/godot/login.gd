cf7aee93f5ebbf6622f5ba0c2f697bcc5782add9
Alejandro Lodes <janosoft@gmail.com>
2026-04-30 21:12:32 -0300

    frontend GODOT - login

---
+++ frontend/godot/login.gd
@@ -0,0 +1,37 @@
+extends Node2D
+
+@onready var email_input = $Panel/email
+@onready var password_input = $Panel/password
+
+var http
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	http = HTTPRequest.new()
+	add_child(http)
+	http.request_completed.connect(_on_request_completed)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_login_php_pressed() -> void:
+	var email = email_input.text
+	var password = password_input.text
+	var data = {"email": email, "password": password}
+	var json_data = JSON.stringify(data)
+	var headers = ["Content-Type: application/json"]
+
+	http.request("http://localhost/CoreDB/backend/php/login.php", headers, HTTPClient.METHOD_POST, json_data)
+	
+func _on_request_completed(result, response_code, headers, body):
+	var response = JSON.parse_string(body.get_string_from_utf8())
+	print (response)
+	
+	if response and response["success"]:
+		print ("Login exitoso: %s - %s" % [response["data"]["email"], response["data"]["codigo"]]);
+	else:
+		print ("Error en el login:", response)
+	
