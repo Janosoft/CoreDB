fa1fb1e9995799ebd36f7be2d659d303131c1e88
Alejandro Lodes <janosoft@gmail.com>
2026-05-06 19:17:07 -0300

    backend laravel

---
+++ backend/laravel/app/Http/Controllers/AuthController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Usuario;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        if (!$request->filled(['email', 'password'])) {
+            // No se recibieron los datos para el inicio de sesión
+            return response()->json(['success' => false, 'error' => 'No se recibieron los datos necesarios.', 'data' => []], 400);
+        }
+
+        $email = $request->input('email');
+        $password = $request->input('password');
+        try {
+            $user = Usuario::where('email', $email)->first();
+
+            if (!$user) {
+                // No se encontró el Usuario en la Base de Datos
+                return response()->json(['success' => false, 'error' => 'Usuario no encontrado.', 'data' => []], 401);
+            }
+
+            if (!Hash::check($password, $user->password)) {
+                // La contraseña no coincide con el hash
+                return response()->json(['success' => false, 'error' => 'Contraseña incorrecta.', 'data' => []], 401);
+            }
+
+            // Inicio de sesión exitoso
+            return response()->json(['success' => true, 'error' => "", 'data' => ['email' => $user->email, 'codigo' => $user->codigo ?? null]]);
+        } catch (\Exception $e) {
+            // Error interno del servidor
+            return response()->json(['success' => false, 'error' => 'Error interno del servidor.', 'data' => []], 500);
+        }
+    }
+}
